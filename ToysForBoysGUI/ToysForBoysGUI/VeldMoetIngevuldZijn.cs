8d6ab4aa717c5c60d16c3a36a9e8f8aa9b53e011
VanTilborgChristophe <christophe.van.tilborg@gmail.com>
2017-05-19 15:57:26 +0200

    ValidationRules

---
+++ ToysForBoysGUI/ToysForBoysGUI/VeldMoetIngevuldZijn.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Globalization;
+
+namespace ToysForBoysGUI
+{
+    class VeldMoetIngevuldZijn : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value == null || value.ToString().Length == 0)
+            {
+                return new ValidationResult(false, "Veld moet ingevuld zijn.");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
